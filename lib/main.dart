46f97ff4d797046a64596748e9064b1c38c8523d
khanhTran <khanhtrantung190894@gmail.com>
2020-12-06 00:44:14 +0700

    add dependeci, random words

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,37 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp(
-      TextInput: new Text("Hello from main"),
-    ));
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  final Widget TextInput;
-  MyApp({this.TextInput});
-  MyAppState createState() => new MyAppState();
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  final wordPair = new WordPair.random();
 
-}
-
-class MyAppState extends State<MyApp> {
-  bool checkBoxValue = false;
-  String actionText = "Default";
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
-      title: "MyAPP",
+      title: "Welcome hello flutter",
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Hello word"),
-          actions: [
-            new IconButton(
-                icon: new Icon(Icons.add),
-                onPressed: () {
-                  setState(() {
-                    actionText = "New Test";
-                  });
-                }),
-            new IconButton(
-                icon: new Icon(Icons.remove),
-                onPressed: () {
-                  setState(() {
-                    actionText = "Default";
-                  });
-                }),
-          ],
+          title: new Text("Welcome"),
         ),
         body: new Center(
-          child: new Column(
-            children: <Widget>[
-              widget.TextInput,
-              new Text(actionText),
-              new Checkbox(
-                  value: checkBoxValue,
-                  onChanged: (bool value) {
-                    setState(() {
-                      checkBoxValue = value;
-                    });
-                  })
-            ],
-          ),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return new Text(WordPair.random().asPascalCase);
   }
 }
