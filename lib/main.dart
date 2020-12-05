78895e217ff7f62b8f1727611b8604c08ac2613c
khanhTran <khanhtrantung190894@gmail.com>
2020-12-06 00:05:59 +0700

    add 2 icon, state less to state full

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,55 @@ void main() => runApp(new MyApp(
       TextInput: new Text("Hello from main"),
     ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final Widget TextInput;
   MyApp({this.TextInput});
-
+  MyAppState createState() => new MyAppState();
   // This widget is the root of your application.
+
+}
+
+class MyAppState extends State<MyApp> {
+  bool checkBoxValue = false;
+  String actionText = "Default";
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return new MaterialApp(
-      title: "MySample",
+      title: "MyAPP",
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("My second app"),
+          title: new Text("Hello word"),
+          actions: [
+            new IconButton(
+                icon: new Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    actionText = "New Test";
+                  });
+                }),
+            new IconButton(
+                icon: new Icon(Icons.remove),
+                onPressed: () {
+                  setState(() {
+                    actionText = "Default";
+                  });
+                }),
+          ],
         ),
         body: new Center(
           child: new Column(
-            children: <Widget>[TextInput, TextInput],
+            children: <Widget>[
+              widget.TextInput,
+              new Text(actionText),
+              new Checkbox(
+                  value: checkBoxValue,
+                  onChanged: (bool value) {
+                    setState(() {
+                      checkBoxValue = value;
+                    });
+                  })
+            ],
           ),
         ),
       ),
