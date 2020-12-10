9e8c9d208c725b15ccd409324759ffd49ee5546c
khanhTran <khanhtrantung190894@gmail.com>
2020-12-10 23:14:37 +0700

    snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,39 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new SnackBarDemo());
+}
+
+class SnackBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: "SnackBar demo",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("SnackBar demo"),
+        ),
+        body: new SnackBarPage(),
+      ),
+    );
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new RaisedButton(
+          onPressed: () {
+            final SnackBar snackBar = new SnackBar(
+              content: new Text("Hey there"),
+              action: new SnackBarAction(label: "Undo", onPressed: () {}),
+            );
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+          child: new Text("Show SnackBar")),
+    );
+  }
+}
