c0da301b40631c2b41db72bc303d0638d5426738
khanhTran <khanhtrantung190894@gmail.com>
2020-12-10 23:27:02 +0700

    tapbar demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new TapBarDemo());
+}
+
+class TapBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      home: new DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            appBar: new AppBar(
+              bottom: new TabBar(tabs: <Widget>[
+                new Tab(icon: new Icon(Icons.directions_bike)),
+                new Tab(icon: new Icon(Icons.directions_boat)),
+                new Tab(icon: new Icon(Icons.directions_bus)),
+              ]),
+              title: new Text("Tapbar demo"),
+            ),
+            body: new TabBarView(children: <Widget>[
+              new Icon(Icons.directions_bike),
+              new Icon(Icons.directions_boat),
+              new Icon(Icons.directions_bus),
+            ]),
+          )),
+    );
+  }
+}
