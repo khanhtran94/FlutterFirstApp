4f652adc7426d01dcfc8056f682591dc7fac5f85
khanhTran <khanhtrantung190894@gmail.com>
2020-12-10 22:53:12 +0700

    video 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-void main() => runApp(new MyApp(
-      TextInput: new Text("Hello from main"),
-    ));
-
-class MyApp extends StatefulWidget {
-  final Widget TextInput;
-  MyApp({this.TextInput});
-  MyAppState createState() => new MyAppState();
-  // This widget is the root of your application.
-
+void main() {
+  runApp(new MyApp());
 }
 
-class MyAppState extends State<MyApp> {
-  bool checkBoxValue = false;
-  String actionText = "Default";
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final appName = "Custom thems";
+
     return new MaterialApp(
-      title: "MyAPP",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Hello word"),
-          actions: [
-            new IconButton(
-                icon: new Icon(Icons.add),
-                onPressed: () {
-                  setState(() {
-                    actionText = "New Test";
-                  });
-                }),
-            new IconButton(
-                icon: new Icon(Icons.remove),
-                onPressed: () {
-                  setState(() {
-                    actionText = "Default";
-                  });
-                }),
-          ],
-        ),
-        body: new Center(
-          child: new Column(
-            children: <Widget>[
-              widget.TextInput,
-              new Text(actionText),
-              new Checkbox(
-                  value: checkBoxValue,
-                  onChanged: (bool value) {
-                    setState(() {
-                      checkBoxValue = value;
-                    });
-                  })
-            ],
+      title: appName,
+      theme: new ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue,
+          accentColor: Colors.cyan[600]),
+      home: new MyHomePage(title: appName),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            "Text with background color",
+            // ignore: deprecated_member_use
+            style: Theme.of(context).textTheme.title,
           ),
         ),
       ),
+      floatingActionButton: new Theme(
+          data: Theme.of(context).copyWith(accentColor: Colors.red),
+          child: new FloatingActionButton(
+            onPressed: null,
+            child: new Icon(Icons.add),
+          )),
     );
   }
 }
