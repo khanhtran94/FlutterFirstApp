110156a62d872a8cd630ce30a7a354619c1e137f
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 23:34:01 +0700

    inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final titile = "Inkwell example";
+  final title = "Inkwell demo";
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: titile,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(titile),
-        ),
-        body: null,
+        title: title,
+        home: MyHomePage(
+          title: title,
+        ));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: MyButton(),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new InkWell(
+      onTap: () {
+        Scaffold.of(context)
+            .showSnackBar(new SnackBar(content: new Text("Tapped")));
+      },
+      child: new Container(
+        padding: new EdgeInsets.all(12),
+        child: new Text("Flat button"),
+        color: Colors.red,
       ),
     );
   }
