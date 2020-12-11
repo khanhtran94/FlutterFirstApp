719ec026a596a8f963005692ae6e4999e06c6b02
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 18:05:44 +0700

    video 15

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,60 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String appTitle = "Drawer dome";
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: appTitle,
+      home: new MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Text("My page!"),
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            new DrawerHeader(
+              child: new Text("Header"),
+              decoration: new BoxDecoration(
+                color: Colors.amber[200],
+              ),
+            ),
+            new ListTile(
+              title: new Text("Item 1"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            new ListTile(
+              title: new Text("Item 1"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
