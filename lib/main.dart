e2a11ed614b7af2525b1b46533d3d1bebe22c319
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 21:03:11 +0700

    video 19 cache image netword;

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String title = "Cached images";
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: CachedNetworkImage(
+            imageUrl: "http://via.placeholder.com/350x350",
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+      ),
+    );
+  }
+}
