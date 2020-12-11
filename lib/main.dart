a9014514fc6702e09298ec536d7a608394a99b1d
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 21:26:59 +0700

    video 19 basic list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final title = "Basic list";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.map),
+              title: new Text("Map"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.photo_album),
+              title: new Text("Photo album"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.phone),
+              title: new Text("Phone"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
