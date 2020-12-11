b3de298d0eaf63f490c7e21f13e4f2465fbdb497
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 23:16:12 +0700

    video 26, gestue

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,56 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final title = "GGesture demo";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+        title: title,
+        home: MyHomePage(
+          title: title,
+        ));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: MyButton(),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new GestureDetector(
+      onTap: () {
+        final snack = new SnackBar(content: new Text("Tapped"));
+        Scaffold.of(context).showSnackBar(snack);
+      },
+      child: new Container(
+        padding: new EdgeInsets.all(12.0),
+        decoration: new BoxDecoration(
+            color: Theme.of(context).buttonColor,
+            borderRadius: new BorderRadius.circular(8.0)),
+        child: new Text("My button"),
+      ),
+    );
+  }
+}
