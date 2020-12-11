472f3698ec491adb6ef52c69abf86645d7b5458f
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 21:34:51 +0700

    video 21, horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,47 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final title = "Horizontal list";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Container(
+          margin: new EdgeInsets.symmetric(vertical: 20.0),
+          height: 200,
+          child: new ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              new Container(
+                width: 160,
+                color: Colors.amber[400],
+              ),
+              new Container(
+                width: 160,
+                color: Colors.red[400],
+              ),
+              new Container(
+                width: 160,
+                color: Colors.blue[400],
+              ),
+              new Container(
+                width: 160,
+                color: Colors.brown[400],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
