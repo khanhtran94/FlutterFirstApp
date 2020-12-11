63656bf63df7997e15648daf007de6b497740cab
khanhTran <khanhtrantung190894@gmail.com>
2020-12-11 22:58:16 +0700

    video 25

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final titile = "GridView example";
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: titile,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(titile),
+        ),
+        body: new GridView.count(
+          crossAxisCount: 3,
+          children: new List.generate(100, (index) {
+            return new Center(
+              child: new Text(
+                "Item $index",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
