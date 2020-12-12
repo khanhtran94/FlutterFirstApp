51017dea746d8790f175d1af79ac93016f56ea07
khanhTran <khanhtrantung190894@gmail.com>
2020-12-12 23:57:11 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final titile = "GridView example";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      title: titile,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(titile),
+        ),
+        body: null,
+      ),
+    );
+  }
+}
