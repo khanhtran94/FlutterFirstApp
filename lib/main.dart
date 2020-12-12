b0c660e85232874038b17feeb45cc4408c5aff7d
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 00:06:11 +0700

    animation imaga

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,51 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+  final titile = "Hero animation example";
 
   @override
   Widget build(BuildContext context) {
+    return new MaterialApp(title: titile, home: new MainScreen());
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Main screen"),
+      ),
+      body: new GestureDetector(
+        child: new Hero(
+            tag: 'imageHero',
+            child: new Image.network("http://via.placeholder.com/350x350")),
+        onTap: () {
+          // todo: question khong hieu sao ham nay lai can (_) nhu nay, vi cac bai truoc thi ko co
+          Navigator.push(context, new MaterialPageRoute(builder: (_) {
+            return new DetailScreen();
+          }));
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     // TODO: implement build
-    return new MaterialApp(
-      title: titile,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(titile),
-        ),
-        body: null,
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Detail screen"),
+      ),
+      body: new GestureDetector(
+        child: new Hero(
+            tag: "imageHero",
+            child: new Image.network("http://via.placeholder.com/150x150")),
+        onTap: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
