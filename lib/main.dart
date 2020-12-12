67821077880a530bd91f4a0182d4a4bdddf8be8c
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 00:35:55 +0700

    fade in animation, opacity, FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: titile,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(titile),
+        title: titile,
+        home: new MainScreen(
+          title: titile,
+        ));
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  final String title;
+  MainScreen({Key key, this.title}) : super(key: key);
+
+  _MainScreenState createState() => new _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.1,
+          duration: new Duration(milliseconds: 500),
+          child: new Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
         ),
-        body: null,
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: "Toogle opacity",
+        child: new Icon(Icons.format_paint),
       ),
     );
   }
