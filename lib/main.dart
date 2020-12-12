3e86c512b337f2451bd37ad9a284529ca20c1f1e
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 02:42:25 +0700

    add menu, action appbar

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,32 @@ class MyApp extends StatelessWidget {
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text(titile),
+          actions: [
+            new IconButton(icon: Icon(Icons.ac_unit_sharp), onPressed: () {}),
+            new IconButton(icon: Icon(Icons.car_rental), onPressed: () {}),
+            new PopupMenuButton<String>(itemBuilder: (BuildContext context) {
+              return [
+                new PopupMenuItem<String>(
+                  child: new IconButton(
+                    color: Colors.black,
+                    icon: new Icon(Icons.directions_bus),
+                    onPressed: () {},
+                  ),
+                ),
+                new PopupMenuItem<String>(
+                    child: new Row(
+                  children: [
+                    new IconButton(
+                      color: Colors.black,
+                      icon: new Icon(Icons.directions_train),
+                      onPressed: () {},
+                    ),
+                    new Text("train")
+                  ],
+                )),
+              ];
+            })
+          ],
         ),
         body: null,
       ),
