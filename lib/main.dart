7816f73de98f5568325e96ecb3d872662cc4a8a1
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 10:25:26 +0700

    state checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,61 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color textColor;
+  bool checkBoxValue;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    textColor = Colors.red;
+    checkBoxValue = false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
-      title: titile,
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text(titile),
+          title: new Text("checkbox"),
         ),
-        body: null,
+        body: new Center(
+          child: new Column(
+            children: [
+              new Text(
+                  "some term and conditions that you may not wanna read and argee to in order to move to next step" +
+                      "aa"),
+              new Row(
+                children: [
+                  new Expanded(
+                      child: new Text(
+                    "Agree to our term ?",
+                    style: new TextStyle(color: textColor),
+                    textAlign: TextAlign.right,
+                  )),
+                  new Checkbox(
+                      value: checkBoxValue,
+                      onChanged: (bool checked) {
+                        setState(() {
+                          checkBoxValue = checked;
+                          if (checked == true) {
+                            textColor = Colors.green;
+                          } else {
+                            textColor = Colors.red;
+                          }
+                        });
+                      })
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
