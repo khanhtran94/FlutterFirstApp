8ae8eb50180fdf977e412dbdb70262d4ccfd24e4
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 10:17:29 +0700

    tab bar controller

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,81 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController tabController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = new TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new MaterialApp(
-      title: titile,
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text(titile),
+          title: new Text("TabBar"),
+          bottom: new TabBar(
+            controller: tabController,
+            tabs: <Widget>[
+              new Tab(
+                icon: new Icon(Icons.home),
+              ),
+              new Tab(
+                icon: new Icon(Icons.history),
+              ),
+              new Tab(
+                icon: new Icon(Icons.trending_down),
+              )
+            ],
+          ),
         ),
-        body: null,
+        body: new TabBarView(controller: tabController, children: <Widget>[
+          new Center(
+            child: new Icon(
+              Icons.home,
+              size: 128.0,
+              color: Colors.blue,
+            ),
+          ),
+          new Center(
+            child: new Icon(
+              Icons.history,
+              size: 128.0,
+              color: Colors.blue,
+            ),
+          ),
+          new Center(
+            child: new Icon(
+              Icons.trending_down,
+              size: 128.0,
+              color: Colors.blue,
+            ),
+          )
+        ]),
+        bottomNavigationBar: new Material(
+          color: Colors.blue,
+          child: new TabBar(
+            tabs: <Widget>[
+              new Tab(
+                icon: new Icon(Icons.home),
+              ),
+              new Tab(
+                icon: new Icon(Icons.history),
+              ),
+              new Tab(
+                icon: new Icon(Icons.trending_down),
+              )
+            ],
+            controller: tabController,
+          ),
+        ),
       ),
     );
   }
