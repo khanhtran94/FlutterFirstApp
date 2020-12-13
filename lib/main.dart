79d5310e33e754b122c6fd4b78f8d54a4938c4a3
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 09:31:13 +0700

    gridview stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,44 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> items = new List<String>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (var i = 0; i < 100; i++) {
+      items.add("Item $i");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: titile,
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text(titile),
+          title: new Text("GridView"),
         ),
-        body: null,
+        body: new GridView.builder(
+          gridDelegate:
+              new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new Card(
+              color: Colors.amberAccent,
+              child: new Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new Text(items[index]),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
