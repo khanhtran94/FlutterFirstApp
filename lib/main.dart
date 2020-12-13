8c8510e1aabd9f420dd8272d98c2a659a69abb06
khanhTran <khanhtrantung190894@gmail.com>
2020-12-13 23:24:19 +0700

    dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,67 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(new MaterialApp(
     home: new Scaffold(
-      backgroundColor: Colors.orange,
-      body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage("images/eog-icon.png"),
-          ),
-          Text(
-            "DevTechie inc",
-            style: TextStyle(
-                fontFamily: 'Satisfy',
-                fontSize: 32,
-                color: Colors.white,
-                fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Learning made easy",
-            style: TextStyle(
-                fontFamily: 'Satisfy',
-                fontSize: 24,
-                color: Colors.white,
-                letterSpacing: 3,
-                fontWeight: FontWeight.bold),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.orange,
-                  ),
-                  title: Text(
-                    "0963192894",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Satisfy',
-                        fontSize: 20.0),
-                  ),
-                )),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-            child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.orange,
-                  ),
-                  title: Text(
-                    "khanhtrantung@gmail.com",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Satisfy',
-                        fontSize: 20.0),
-                  ),
-                )),
-          ),
-        ],
-      )),
+      backgroundColor: Colors.orange.shade300,
+      appBar: new AppBar(
+        title: new Text(
+          "Dice game",
+          style: TextStyle(color: Colors.blueAccent),
+        ),
+        backgroundColor: Colors.orange.shade50,
+      ),
+      body: DiceGame(),
     ),
   ));
+}
+
+class DiceGame extends StatefulWidget {
+  @override
+  _DiceGameState createState() => _DiceGameState();
+}
+
+class _DiceGameState extends State<DiceGame> {
+  int lDice = Random().nextInt(6) + 1;
+  int rDice = Random().nextInt(6) + 1;
+
+  void updateDice() {
+    setState(() {
+      lDice = Random().nextInt(6) + 1;
+      rDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Image(
+              image: AssetImage("images/dice$lDice.png"),
+            ),
+            onPressed: () {
+              updateDice();
+            },
+          )),
+          Expanded(
+              child: FlatButton(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onPressed: () {
+              updateDice();
+            },
+            child: Image(
+              image: AssetImage("images/dice$rDice.png"),
+            ),
+          ))
+        ],
+      ),
+    );
+  }
 }
