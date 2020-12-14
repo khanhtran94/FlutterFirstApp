bd6c38905bcc6f2206d96b8cf79a9caeeb1c5386
khanhTran <khanhtrantung190894@gmail.com>
2020-12-15 00:17:10 +0700

    done pie chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+final List<List<CircularStackEntry>> _quarterlyProfitPieData = [
+  <CircularStackEntry>[
+    new CircularStackEntry(<CircularSegmentEntry>[
+      new CircularSegmentEntry(500.0, Colors.red[200], rankKey: "Q1"),
+      new CircularSegmentEntry(1500.0, Colors.yellow[200], rankKey: "Q2"),
+      new CircularSegmentEntry(200.0, Colors.blue[200], rankKey: "Q3"),
+      new CircularSegmentEntry(1500.0, Colors.green[200], rankKey: "Q4"),
+    ], rankKey: "Quarterly profit"),
+  ],
+  <CircularStackEntry>[
+    new CircularStackEntry(<CircularSegmentEntry>[
+      new CircularSegmentEntry(200.0, Colors.red[200], rankKey: "Q1"),
+      new CircularSegmentEntry(2500.0, Colors.yellow[200], rankKey: "Q2"),
+      new CircularSegmentEntry(100.0, Colors.blue[200], rankKey: "Q3"),
+      new CircularSegmentEntry(1100.0, Colors.green[200], rankKey: "Q4"),
+    ], rankKey: "Quarterly profit"),
+  ],
+  <CircularStackEntry>[
+    new CircularStackEntry(<CircularSegmentEntry>[
+      new CircularSegmentEntry(300.0, Colors.red[200], rankKey: "Q1"),
+      new CircularSegmentEntry(1100.0, Colors.yellow[200], rankKey: "Q2"),
+      new CircularSegmentEntry(400.0, Colors.blue[200], rankKey: "Q3"),
+      new CircularSegmentEntry(1500.0, Colors.green[200], rankKey: "Q4"),
+    ], rankKey: "Quarterly profit"),
+  ],
+  <CircularStackEntry>[
+    new CircularStackEntry(<CircularSegmentEntry>[
+      new CircularSegmentEntry(500.0, Colors.red[200], rankKey: "Q1"),
+      new CircularSegmentEntry(500.0, Colors.yellow[200], rankKey: "Q2"),
+      new CircularSegmentEntry(400.0, Colors.blue[200], rankKey: "Q3"),
+      new CircularSegmentEntry(1500.0, Colors.green[200], rankKey: "Q4"),
+    ], rankKey: "Quarterly profit"),
+  ]
+];
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<AnimatedCircularChartState> _chartKey =
+      GlobalKey<AnimatedCircularChartState>();
+  final _charSize = const Size(300.0, 300.0);
+  int sampleIndex = 0;
+
+  void _cycleSamples() {
+    setState(() {
+      sampleIndex++;
+      List<CircularStackEntry> data = _quarterlyProfitPieData[sampleIndex % 3];
+      _chartKey.currentState.updateData(data);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new MaterialApp(
-      title: titile,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(titile),
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Char circle"),
+      ),
+      body: Center(
+        child: AnimatedCircularChart(
+          initialChartData: _quarterlyProfitPieData[0],
+          key: _chartKey,
+          size: _charSize,
+          chartType: CircularChartType.Pie,
         ),
-        body: null,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _cycleSamples,
+        child: Icon(Icons.refresh),
       ),
     );
   }
