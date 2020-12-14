2525c5cf3bd8494ea42d4501382c9d1b6fa8f5aa
khanhTran <khanhtrantung190894@gmail.com>
2020-12-15 00:27:53 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+  final titile = "flutter_staggered_grid_view example";
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,26 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text(titile),
         ),
-        body: null,
+        body: Padding(
+          padding: const EdgeInsets.all(4),
+          child: StaggeredGridView.countBuilder(
+            crossAxisCount: 4,
+            itemCount: 8,
+            itemBuilder: (BuildContext context, int index) => Container(
+              color: Colors.green,
+              child: Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text("$index"),
+                ),
+              ),
+            ),
+            staggeredTileBuilder: (int index) =>
+                StaggeredTile.count(2, index.isEven ? 2 : 1),
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+          ),
+        ),
       ),
     );
   }
