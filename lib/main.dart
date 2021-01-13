7551c26e907ba4f1c1dc68274749529ca511fcf3
khanhTran <khanhtrantung190894@gmail.com>
2021-01-13 22:57:31 +0700

    sql

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,112 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+import 'dog.dart';
 
-class MyApp extends StatelessWidget {
-  final titile = "GridView example";
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final Future<Database> database = openDatabase(
+    join(await getDatabasesPath(), 'doggie_database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          "CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)");
+    },
+    version: 1,
+  );
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new MaterialApp(
-      title: titile,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(titile),
-        ),
-        body: null,
-      ),
+  Future<void> insertDog(Dog dog) async {
+    final Database db = await database;
+    await db.insert(
+      'dogs',
+      dog.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+
+    final fido = Dog(id: 0, name: 'Fido', age: 30);
+    await insertDog(fido);
+  }
+
+  Future<List<Dog>> dogs() async {
+    // Get a reference to the database.
+    final Database db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return Dog(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        age: maps[i]['age'],
+      );
+    });
+  }
+
+  print(await dogs()); // Prints a list that include Fido.
+
+  Future<void> updateDog(Dog dog) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Update the given Dog.
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      // Ensure that the Dog has a matching id.
+      where: "id = ?",
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [dog.id],
     );
   }
+
+  await updateDog(Dog(
+    id: 0,
+    name: 'Fido',
+    age: 42,
+  ));
+
+  Future<void> deleteDog(int id) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Remove the Dog from the Database.
+    await db.delete(
+      'dogs',
+      // Use a `where` clause to delete a specific dog.
+      where: "id = ?",
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [id],
+    );
+  }
+
+  var fido = Dog(
+    id: 0,
+    name: 'Fido',
+    age: 35,
+  );
+
+  await insertDog(fido);
+
+  fido = Dog(
+    id: fido.id,
+    name: fido.name,
+    age: fido.age + 7,
+  );
+  await updateDog(fido);
+
+  print(await dogs());
+
+  // await deleteDog(fido.id);
+
+  print(await dogs());
+
+  ByteData data = await rootBundle.load(join('assets', 'doggie_database.db'));
+  List<int> bytes =
+      data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  print("${bytes}");
 }
