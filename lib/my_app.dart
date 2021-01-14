25e5e9eda4ffa080c408c83c5ffbb74c5a16ff9c
khanhTran <khanhtrantung190894@gmail.com>
2021-01-14 22:58:05 +0700

    statefulwigdet

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   String name;
   int age;
   MyApp({this.name, this.age});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _name = '';
+  final nameEdittingController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Home app",
+      title: 'Home',
       home: Scaffold(
         body: Center(
-          child: Text(
-            '${this.name} - ${this.age}',
-            style: TextStyle(color: Colors.amberAccent, fontSize: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: TextField(
+                    controller: nameEdittingController,
+                    onChanged: (text) {
+                      this.setState(() {
+                        // when state change => build() reload widget
+                        _name = text;
+                      });
+                    },
+                    decoration: InputDecoration(
+                        alignLabelWithHint: true,
+                        border: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20))),
+                        labelText: 'Input name'),
+                  )),
+              Text(
+                _name,
+                style: TextStyle(fontSize: 30, color: Colors.amber),
+              )
+            ],
           ),
         ),
       ),
     );
   }
+}
