df7dd10f4291b8db4e3bb1dccf1043660a733716
khanhTran <khanhtrantung190894@gmail.com>
2021-01-15 23:01:36 +0700

    replce 1

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,66 +1,69 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  final _contentController = new TextEditingController();
-  final _priceController = new TextEditingController();
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  String _content = '';
-  double _price = 0;
+  final _nameEditCon = new TextEditingController();
+  final _telEditCon = new TextEditingController();
+  final GlobalKey<ScaffoldState> _globalKeyScaffold = new GlobalKey();
+
+  String _name = '';
+  int _tel = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Home app",
-      home: Scaffold(
-          key: _scaffoldKey,
+        title: "Home app",
+        home: Scaffold(
+          key: _globalKeyScaffold,
           body: SafeArea(
-              minimum: EdgeInsets.only(left: 20, right: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(top: 20),
-                    child: TextField(
-                      controller: _contentController,
-                      decoration: InputDecoration(
-                          labelText: 'Input content',
-                          border: OutlineInputBorder(
-                              borderRadius: const BorderRadius.all(
-                                  const Radius.circular(20)))),
-                      onChanged: (text) {
-                        _content = text;
-                      },
-                    ),
+            minimum: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: TextField(
+                    controller: _nameEditCon,
+                    decoration: InputDecoration(
+                        labelText: 'input name',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(const Radius.circular(20)))),
+                    onChanged: (text) {
+                      _name = text;
+                    },
                   ),
-                  Container(
-                    padding: EdgeInsets.only(top: 20),
-                    child: TextField(
-                      controller: _priceController,
-                      decoration: InputDecoration(
-                          labelText: 'Input content',
-                          border: OutlineInputBorder(
-                              borderRadius: const BorderRadius.all(
-                                  const Radius.circular(20)))),
-                      onChanged: (text) {
-                        _price = double.tryParse(text) ?? 0;
-                      },
-                    ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: TextField(
+                    controller: _telEditCon,
+                    decoration: InputDecoration(
+                        labelText: 'input name',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(const Radius.circular(20)))),
+                    onChanged: (text) {
+                      _tel = int.tryParse(text) ?? 0;
+                    },
                   ),
-                  FlatButton(
-                      onPressed: () {
-                        print('$_content -- $_price');
-                        _scaffoldKey.currentState.showSnackBar(SnackBar(
-                          content: Text('$_content -- $_price'),
-                          duration: Duration(seconds: 5),
-                        ));
-                      },
-                      textColor: Colors.red,
-                      child: Icon(
-                        Icons.add,
-                        size: 50,
-                      )),
-                ],
-              ))),
-    );
+                ),
+                FlatButton(
+                  onPressed: () {
+                    print('$_name -- $_tel');
+                    _globalKeyScaffold.currentState.showSnackBar(SnackBar(
+                      content: Text('$_name -- $_tel'),
+                      duration: Duration(seconds: 5),
+                    ));
+                  },
+                  child: Icon(
+                    Icons.ac_unit_outlined,
+                    size: 50,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
