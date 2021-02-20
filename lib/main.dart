217e53381c79e08888540a5285d2d1849f4f7672
khanhTran <khanhtrantung190894@gmail.com>
2021-02-20 16:06:33 +0700

    init project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
-import 'package:first_app/my_app.dart';
 import 'package:flutter/material.dart';
-import 'my_app.dart';
 
 void main() {
   runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great place',
+      theme: ThemeData(
+          primaryColor: Colors.indigo, accentColor: Colors.amberAccent),
+    );
+  }
 }
